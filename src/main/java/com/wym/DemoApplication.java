1d8c63ac6783bf31c82023a69dbd3241f2bb1c52
wym <879742849@qq.com>
2019-12-18 09:27:51 +0800

    dev1.0

--- src/main/java/com/wym/DemoApplication.java
+++ src/main/java/com/wym/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println();
         System.out.println("dev");
+        System.out.println("HEL");
     }
 
 }
